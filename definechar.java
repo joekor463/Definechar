6e0f2ba7bf4b31139892aa4b85723be9c9bbfed0
Evgeny Korobenkov <joekor463@gmail.com>
2020-05-08 21:25:28 +0800

    define char

---
+++ definechar.java
@@ -0,0 +1,17 @@
+public class definechar {
+    public static void main(String [] args){
+        char choice = 'A';
+        switch (choice){
+            case 'A': System.out.println ( 'A' );
+            break;
+            case 'B': System.out.println ( 'B' );
+                break;
+            case 'C': System.out.println ( 'C' );
+                break;
+            case 'D': System.out.println ( 'D' );
+                break;
+            case 'E': System.out.println ( 'E' );
+                break;
+        }
+    }
+}
